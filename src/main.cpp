bfc3f106ef7f4ccb32dc78cb04a545515f0478e7
Ari Kanbur <152089347+AKminer2@users.noreply.github.com>
2024-06-13 08:47:51 -0700

    add --version option

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ enum ErrorType
 
 void printUsage();
 void printError(ErrorType error, std::map<std::string,std::string> data);
+void printVersion();
 
 int main(int argc, char **argv)
 {
@@ -19,14 +20,18 @@ int main(int argc, char **argv)
     {
         
         // this is the option that the user inputs - paperpass [option] [parameters]
-        std::string option = argv[1];
+        const std::string option = argv[1];
 
         // print a help page
         if (strcmp(argv[1], "--help") == 0)
         {
             printUsage();
         }
-
+        // --version
+        else if (strcmp(argv[1], "--version") == 0)
+        {
+            printVersion();
+        }
         // unknown option error
         else
         {
@@ -41,6 +46,8 @@ int main(int argc, char **argv)
         // if no option is supplied, simply print the help page
         printUsage();
     }
+
+    return 0;
 }
 
 // print a help page
@@ -81,4 +88,15 @@ void printError(
         printUsage();
         break;
     }
+}
+
+void printVersion()
+{
+    const std::string VERSION = (std::string)
+                                "paperpass v0.1.0  Copyright (C) 2024  Ari Kanbur\n"
+                                "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n"
+                                "This is free software, and you are welcome to redistribute it\n"
+                                "under certain conditions; type `show c' for details.\n";
+
+    std::cout << VERSION;
 }
